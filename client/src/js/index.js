6516de69ac17e52d0f45b4f53b57beaffeee9a10
Joseph Lara <vipercar233@gmail.comgit config --global user.name Joseph>
2022-11-14 22:46:53 -0600

    Configured application to use IndexedDB API

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,6 +1,7 @@
 // Import modules
 import './form';
 import './submit';
+import { initDb } from './database';
 
 // Import CSS files
 import '../css/index.css';
@@ -14,6 +15,7 @@ import Dog from '../images/dog.png';
 
 // Add images on load
 window.addEventListener('load', function () {
+  initDb();
   document.getElementById('logo').src = Logo;
   document.getElementById('bearThumbnail').src = Bear;
   document.getElementById('dogThumbnail').src = Dog;
